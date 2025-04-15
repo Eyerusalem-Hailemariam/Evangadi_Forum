cedd62b519280b95c540b84bad04251ddf2c6830
Eyerusalem-Hailemariam <eyerusalem008@gmail.com>
2025-04-15 13:18:05 +0300

    login page

--- Evangadi_Forum_Frontend/vite-project/src/Pages/Login.jsx
+++ Evangadi_Forum_Frontend/vite-project/src/Pages/Login.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
+import { useRef } from 'react'
+import axios from '../axiosConfig'
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
+
+  const Navigate = useNavigate()
+  const emailDom = useRef(null)
+  const passwordDom = useRef(null)
+
+  async function handleSubmit(e){
+    e.preventDefault()
+    const emailValue = emailDom.current.value
+    const passwordValue = passwordDom.current.value
+
+    if(emailValue === "" || passwordValue === ""){
+        alert("Please fill all the fields")
+        return
+    }
+
+    try {
+        const response = await axios.post('/user/login', {
+            "email" : emailValue,
+            "password" : passwordValue
+        });
+        alert("login successfully")
+
+        localStorage.setItem('token', response.data.token)
+        console.log(response.data)
+    } catch (error) {
+        alert(error?.response?.data?.message)
+        console.log(error)
+    }
+}
+
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <section>
+     <form onSubmit={handleSubmit}>
+        <div>
+            <span>email </span>
+            <input ref={emailDom} type="email" placeholder='email'/>
+        </div>
+        <div>
+            <span>password </span>
+            <input ref={passwordDom} type="password" placeholder='password'/>
+        </div>
+
+        <button type="submit">Login</button>
+        </form>
+    </section>
   )
 }
 
