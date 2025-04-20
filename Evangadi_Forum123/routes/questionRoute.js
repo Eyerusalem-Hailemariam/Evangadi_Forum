e61d9a5ebb86bb4a72a0cc7eab327dc0b44c9b56
Eyerusalem-Hailemariam <140660877+Eyerusalem-Hailemariam@users.noreply.github.com>
2025-04-20 14:54:47 +0300

    Delete Evangadi_Forum123 directory

--- Evangadi_Forum123/routes/questionRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.get('/all-questions', authMiddleware, (req, res) => {
-    res.json({ message: 'This is the questions route' });
-});
-
-module.exports = router;
